a691a9fb53c0455ae57e04472335923f11c90558
alexia1003 <145763243+alexia1003@users.noreply.github.com>
2023-12-12 10:53:17 +0200

    Update Carte.cpp

--- Lab3_Carte/Carte.cpp
+++ Lab3_Carte/Carte.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // init. var. statice
 int Carte::numarInstante = 0;  
 
-Carte::Carte(const std::string& autor, const std::string& titlu, const std::string& ISBN, double pret)
+Carte::Carte(const string& autor, const string& titlu, const string& ISBN, double pret)
     : autor(autor), titlu(titlu), ISBN(ISBN), pret(pret) {
     ++numarInstante;  
     // nr. de instante la fiecare creare a unei carti
