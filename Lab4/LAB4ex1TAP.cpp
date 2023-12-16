ced7bb163c72f6bbc23538b21dbcf9d1ad03d322
alexia1003 <145763243+alexia1003@users.noreply.github.com>
2023-12-16 13:24:44 +0200

    Update LAB4ex1TAP.cpp

--- Lab4/LAB4ex1TAP.cpp
+++ Lab4/LAB4ex1TAP.cpp
@@ -3,37 +3,59 @@
 
 // Supraincarcarea operatorilor
 Fractie Fractie::operator+(const Fractie& other) const {
-    return this->aduna(other);
+    Fractie rezultat;
+    rezultat.numarator = this->numarator * other.numitor + other.numarator * this->numitor;
+    rezultat.numitor = this->numitor * other.numitor;
+    return rezultat;
+    //return this->aduna(other);
 }
 
 Fractie Fractie::operator-(const Fractie& other) const {
-    return this->scade(other);
+    Fractie rezult;
+    rezult.numarator = this->numarator * other.numitor - other.numarator * this->numitor;
+    rezult.numitor = this->numitor * other.numitor;
+    return rezult;
+    //return this->scade(other);
 }
 
 Fractie Fractie::operator*(const Fractie& other) const {
-    return this->inmulteste(other);
+    Fractie rez;
+    rez.numarator = this->numarator * other.numarator;
+    rez.numitor = this->numitor * other.numitor;
+    return rez;
+    //return this->inmulteste(other);
 }
 
 Fractie Fractie::operator/(const Fractie& other) const {
-    return this->imparte(other);
+    Fractie r;
+    r.numarator = this->numarator * other.numitor;
+    r.numitor = this->numitor * other.numarator;
+    return r;
+    //return this->imparte(other);
 }
 
 bool Fractie::operator<(const Fractie& other) const {
+    return (this->numarator * other.numitor) < (other.numarator * this->numitor);
 }
 
 bool Fractie::operator>(const Fractie& other) const {
+    return (this->numarator * other.numitor) > (other.numarator * this->numitor);
 }
 
 bool Fractie::operator<=(const Fractie& other) const {
+    return (this->numarator * other.numitor) <= (other.numarator * this->numitor);
 }
 
 bool Fractie::operator>=(const Fractie& other) const {
+    return (this->numarator * other.numitor) >= (other.numarator * this->numitor);
 }
 
 bool Fractie::operator!=(const Fractie& other) const {
+    return (this->numarator * other.numitor) != (other.numarator * this->numitor);
 }
 
 bool Fractie::operator==(const Fractie& other) const {
+    return (this->numarator * other.numitor) == (other.numarator * this->numitor);
 }
 
 // Supraincarcare functii prietene
@@ -54,19 +76,25 @@ Fractie operator/(const Fractie& f1, const Fractie& f2) {
 }
 
 bool operator<(const Fractie& f1, const Fractie& f2) {
+    return (f1.numarator * f2.numitor) < (f2.numarator * f1.numitor);
 }
 
 bool operator>(const Fractie& f1, const Fractie& f2) {
+    return (f1.numarator * f2.numitor) > (f2.numarator * f1.numitor);
 }
 
 bool operator<=(const Fractie& f1, const Fractie& f2) {
+    return (f1 < f2) || (f1 == f2);
 }
 
 bool operator>=(const Fractie& f1, const Fractie& f2) {
+    return (f1 > f2) || (f1 == f2);
 }
 
 bool operator!=(const Fractie& f1, const Fractie& f2) {
+    return !(f1 == f2);
 }
 
 bool operator==(const Fractie& f1, const Fractie& f2) {
+    return (f1.numarator * f2.numitor) == (f2.numarator * f1.numitor);
 }
